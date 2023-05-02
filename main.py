4aedc51a40927d4d7aa90db28931a498b57ea076
Jay Gholizadeh <jgolizadeh@gmail.com>
2023-05-02 23:34:31 +0100

    added the inference data to main file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Put the code for your API here.
 from fastapi import FastAPI
 # BaseModel from Pydantic is used to define data objects.
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 import json
+
+from starter.ml.data import process_data
 
 
 
@@ -10,23 +12,22 @@ import json
 app = FastAPI()
 
 class IneferenceInput(BaseModel):
-    age: int 
-    workclass:str
-    fnlgt:int
-    education:str
-    education-num:int
-    marital-status:str
-    occupation:str
-    relationship:str
-    race:str
-    sex:str
-    capital-gain:int
-    capital-loss:int
-    hours-per-week:int
-    native-country:str
+    age: int = Field(example=39)
+    workclass:str=Field(example='State-gov')
+    fnlgt:int = Field(example=77516)
+    education:str = Field(example='Bachelors')
+    education-num:int = Field(example=13)
+    marital-status:str = Field(example='Never-married')
+    occupation:str = Field(example='Adm-clerical')
+    relationship:str = Field(example='Not-in-family')
+    race:str = Field(example='white')
+    sex:str = Field(example='Male')
+    capital-gain:int = Field(example=2174)
+    capital-loss:int = Field(example=0)
+    hours-per-week:int = Field(example=40)
+    native-country:str = Field(example='United-States')
     
     
-
 @app.get('/')
 def greeting():
 
@@ -34,12 +35,15 @@ def greeting():
 
 
 @app.post('/predict/{item_id}')
-async def predict(item_id:int, body: IneferenceInput):
+async def predict(item_id:int, data: IneferenceInput):
+      results = {"item_id": item_id, "data": data}
+      # convert data into numpy array as input for prediction
+      data_array = np.array(data.dict().values())
+      # import the model and encoders
+      
 
 
-     
-
-    return 
+    return results
 
 
 
